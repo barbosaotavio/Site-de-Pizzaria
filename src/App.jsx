8376767ac3751a283714d70957510e37999c271f
otavio <barbosaotavio293@gmail.com>
2024-10-22 02:39:20 -0300

    site de bebidas

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Bebidas from './Components/Bebidas'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+       <h1>BAR DO MANEL</h1>
+       <h2>Ecolha suas bebidas!</h2>
+  <Bebidas/>
+  
+    </div>
+   )
+}
+
+export default App
---
+++ src/Components/Bebidas.jsx
@@ -0,0 +1,17 @@
+function bebidas () {
+    return (
+     <div>
+        <ul>
+            <li>
+               <li>Brahma</li>
+               <li>Skol</li>
+               <li>Antartica</li>
+               <li>Império</li>
+               <li>Estella</li>
+            </li>
+        </ul>
+     </div>
+    )
+}
+
+export default bebidas
